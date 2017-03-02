346eb7d2578a9d0a525f7d2777405006affed6ee
Abhay Chrungoo <abhay@ziraffe.io>
2017-03-02 14:23:30 +0000

    Feature: Allow  snapshotting a build result

---
+++ resources/com/lbg/workflow/sandbox/js/snapshot.js
@@ -0,0 +1,86 @@
+var system = require('system');
+var env = system.env;
+
+var resourceWait  = 300,
+    maxRenderWait = 10000,
+    url           = 'http://jenkins.sandbox.extranet.group/jenkins2/j_acegi_security_check';
+
+var page          = require('webpage').create(),
+    count         = 5,
+    forcedRenderTimeout,
+    transitions	  = 0,
+    renderTimeout;
+var buildPath = env['BUILD_PATH']
+var jenkinsuser = env['JENKINS_USER']
+var jenkinspass = env['JENKINS_PASS']
+var imagefile = env['IMAGEFILE']
+console.log('buildPath: ' + buildPath)
+var postBody = 'j_username='+ jenkinsuser  +'&j_password='+ jenkinspass +'&remember_me=on&json=init&from=/' + buildPath + 'display/redirect'
+
+
+page.viewportSize = { width: 1024, height : 50000 };
+function doRender(filename) {
+	console.log('rendering png ' + filename);
+
+	page.zoomFactor = 1;
+	page.clipRect = {
+		top: 8,
+		left: 0,
+		width: 1024,
+		height: 600
+	};
+    page.render(filename, {format: 'png', quality: '100'});
+};
+
+page.onInitialized = function() {
+     if(page.injectJs('node_modules/babel-polyfill/browser.js')){
+         console.log("Babel-Polyfill loaded");
+     }    
+};
+
+page.onResourceRequested = function (req) {
+   count += 1;
+   console.log('> ' + req.id + ' - ' + req.url);
+   clearTimeout(renderTimeout);
+};
+
+page.onResourceReceived = function (res) {
+    if (!res.stage || res.stage === 'end') {
+        count -= 1;
+        console.log(res.id + ' ' + res.status + ' - ' + res.url);
+        if (count === 0) {
+            renderTimeout = setTimeout(doRender('onReceived_' + imagefile), resourceWait);
+        }
+    }
+};
+
+ page.onLoadFinished = function(status){
+	console.log('Load Finished');
+	 if (status !== "success") {
+        console.log('Unable to load url');
+        phantom.exit();
+     } else if (transitions === 0){
+    	console.log('Remaining Transitions ' + transitions);
+     	doRender('onLoaded_' + imagefile);
+    	//phantom.exit();
+     } else { 
+     	console.log('Remaining Transitions ' + transitions);
+     	transitions -= 1;
+     }
+};
+ page.onPageReady = function(status){
+       	doRender('onReady_' + imagefile);
+};   
+
+page.open(url, 'POST', postBody, function (status) {
+    if (status !== "success") {
+        console.log('Unable to load url');
+        phantom.exit();
+    } else {
+   	 console.log('success');
+        forcedRenderTimeout = setTimeout(function () {
+           doRender(imagefile);    
+           phantom.exit();  
+        }, maxRenderWait);
+    }
+});
--- src/com/lbg/workflow/sandbox/Utils.groovy
+++ src/com/lbg/workflow/sandbox/Utils.groovy
@@ -24,3 +24,28 @@ def friendlyName(String branchName, int maxsize){
 		return buffer
 	}
 }
+
+def snapshotStatus(String imagefile){
+
+	def code = libraryResource 'com/lbg/workflow/sandbox/js/snapshot.js'
+	writeFile file: 'snapshot.js', text: code
+	sh "cat snapshot.js"
+	def buildPath = env.BUILD_URL.replace(env.JENKINS_URL, '')
+	echo buildPath
+	echo env.BUILD_URL
+	echo env.JENKINS_URL
+	echo "gooooooooooooooooooooooooooooooooooooooooooooo"
+	withCredentials([
+		usernamePassword(credentialsId: 'bluemix-global-deployer',
+		passwordVariable: 'JENKINS_PASS',
+		usernameVariable: 'JENKINS_USER')
+	]) {
+		withEnv([
+			"BUILD_PATH=${buildPath}",
+			"IMAGEFILE=${imagefile}"
+		]) { sh """npm install phantomjs@2.1.7  babel-polyfill@6.23.0 && \
+                   node_modules/.bin/phantomjs snapshot.js
+                """  }
+	}
+
+}
