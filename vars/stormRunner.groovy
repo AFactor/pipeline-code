865fcc36047bdec0408c35a2e43eee79ec69ccd5
Neil Tingley <neil@winder-it.co.uk>
2017-04-18 15:59:26 +0100

    Fixed typo in stormRunner.groovy , parameterised the jar file name to upload to storm

--- vars/stormRunner.groovy
+++ vars/stormRunner.groovy
@@ -16,6 +16,7 @@ def call(Closure body) {
     def stormRemoteUser = config.remoteUser
     def stormSSHUSer = config.sshUser
     def stormTopologyClass = config.stormTopClass
+    def stormJarName = config.pathToJar
     def stormTopologyName = config.stormTopName
     def sshOpts = '-o StrictHostKeyChecking=no'
 
@@ -25,9 +26,9 @@ def call(Closure body) {
     sshagent(credentials: [stormSSHUSer]) {
         sh """
             ssh ${sshOpts}  ${stormRemoteUser}@${stormNode} "mkdir deployments || true"
-            scp audit/target/audit-*-jar-with-dependencies.jar ${stormRemoteUser}@${stormNode}:deployments
-            jar=`ssh ${sshOpts}${stormRemoteUser}@${stormNode} "ls -t /home/${stormRemoteUser}/deployments/* | head -1"`
-            ssh ${sshOpts} ${stormRemoteUser}@${stormNode} "storm kill ${stormTopologyName } || true
+            scp ${stormJarName} ${stormRemoteUser}@${stormNode}:deployments
+            jar=`ssh ${sshOpts} ${stormRemoteUser}@${stormNode} "ls -t /home/${stormRemoteUser}/deployments/* | head -1"`
+            ssh ${sshOpts} ${stormRemoteUser}@${stormNode} "storm kill ${stormTopologyName } || true"
             ssh ${sshOpts} ${stormRemoteUser}@${stormNode} "storm jar \$jar ${stormTopologyClass} sbdev cluster"
               """
     }
