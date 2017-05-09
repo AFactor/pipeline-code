848d735a39aa11119b5e8e4f759e75b8668cb7ae
Ram Guda <rguda@sapient.com>
2017-05-09 12:43:16 +0100

    extend bluemix withenv lib to support service and global bluemix config

--- tests/vars/bluemixWithEnvSpec.groovy
+++ tests/vars/bluemixWithEnvSpec.groovy
@@ -4,16 +4,16 @@ import support.PipelineSpockTestBase
 
 class bluemixWithEnvSpec extends PipelineSpockTestBase {
 
-    def "build bluemix withenv configuration"() {
+    def "build environment variable for given bluemix config"() {
         given:
         HashMap bluemix = [
-                "org": "org-name",
-                "domain": "lbg.eu-gb.mybluemix.net",
-                "api": "api.lbg.eu-gb.bluemix.net",
+                "org"        : "org-name",
+                "domain"     : "lbg.eu-gb.mybluemix.net",
+                "api"        : "api.lbg.eu-gb.bluemix.net",
                 "credentials": "bluemix-global-deployer",
-                "env": "Test",
-                "disk": "256M",
-                "memory": "256M"]
+                "env"        : "Test",
+                "disk"       : "256M",
+                "memory"     : "256M"]
 
         when:
         def script = loadScript("vars/bluemixWithEnv.groovy")
@@ -24,12 +24,44 @@ class bluemixWithEnvSpec extends PipelineSpockTestBase {
         assertJobStatusSuccess()
         assert result != null
         assert result == [
-        "BM_API=${bluemix.api}",
-        "BM_DOMAIN=${bluemix.domain}",
-        "BM_ORG=${bluemix.org}",
-        "BM_ENV=${bluemix.env}",
-        "DISK=${bluemix.disk}",
-        "MEMORY=${bluemix.memory}"
+                "BM_API=${bluemix.api}",
+                "BM_DOMAIN=${bluemix.domain}",
+                "BM_ORG=${bluemix.org}",
+                "BM_ENV=${bluemix.env}",
+                "DISK=${bluemix.disk}",
+                "MEMORY=${bluemix.memory}"
+        ]
+    }
+
+    def "build environment variables for given service and global bluemix config"() {
+        given:
+        HashMap serviceBluemixConfig = ["deployable": "j2/service-name", "APP": "service-name"]
+        HashMap globalBluemixConfig = [
+                "org"        : "org-name",
+                "domain"     : "lbg.eu-gb.mybluemix.net",
+                "api"        : "api.lbg.eu-gb.globalBluemixConfig.net",
+                "credentials": "globalBluemixConfig-global-deployer",
+                "env"        : "Test",
+                "disk"       : "256M",
+                "memory"     : "256M"]
+
+        when:
+        def script = loadScript("vars/bluemixWithEnv.groovy")
+        def result = script.call(serviceBluemixConfig, globalBluemixConfig)
+
+        then:
+        printCallStack()
+        assertJobStatusSuccess()
+        assert result != null
+        assert result == [
+                "BM_API=${globalBluemixConfig.api}",
+                "BM_DOMAIN=${globalBluemixConfig.domain}",
+                "BM_ORG=${globalBluemixConfig.org}",
+                "BM_ENV=${globalBluemixConfig.env}",
+                "DISK=${globalBluemixConfig.disk}",
+                "MEMORY=${globalBluemixConfig.memory}",
+                "deployable=j2/service-name",
+                "APP=service-name"
         ]
     }
 }
--- vars/bluemixWithEnv.groovy
+++ vars/bluemixWithEnv.groovy
@@ -5,3 +5,13 @@ def call(bluemixConfig) {
 	def envs = utils.buildBluemixEnv(bluemixConfig)
 	return utils.toWithEnv(envs)
 }
+
+
+def call(serviceBluemixConfig, globalBluemixConfig) {
+    UtilsBluemix utils = new UtilsBluemix()
+    def envs = utils.buildBluemixEnv(globalBluemixConfig)
+    for (e in serviceBluemixConfig) {
+        envs[e.key] = e.value
+    }
+    return utils.toWithEnv(envs)
+}
