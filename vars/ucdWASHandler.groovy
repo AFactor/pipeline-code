1cb84a6739e252e0c4314df15dc1f7ba7692e8f2
Conor Hawkshaw <chawkshaw@sapient.com>
2017-07-14 15:39:50 +0100

    add handler for was paramaters

--- vars/ucdWASHandler.groovy
+++ vars/ucdWASHandler.groovy
@@ -1,12 +1,12 @@
 import com.lbg.workflow.sandbox.deploy.UtilsUCD
 
 
-def call(String token, String name) {
+def call(String name) {
     def versionsChoice = ''
     node('lbg_slave') {
         def ucdTokenKey = 'UC_TOKEN_MCA'
         withCredentials([string(credentialsId: ucdTokenKey, variable: 'ucdToken')]) {
-            versionsChoice = UtilsUCD.ucdMCAComponentVersion(token, name)
+            versionsChoice = UtilsUCD.ucdMCAComponentVersion(ucdToken, name)
         }
     }
     return versionsChoice
