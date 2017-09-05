04e585d0a3b117769a294887ebd3e3944af0108f
Vamegh Hedayati <vhedayati@sapient.com>
2017-09-05 17:21:15 +0100

    changed environment to deployContext.env
    	modified:   src/com/lbg/workflow/sandbox/deploy/UtilsUCD.groovy

--- src/com/lbg/workflow/sandbox/deploy/UtilsUCD.groovy
+++ src/com/lbg/workflow/sandbox/deploy/UtilsUCD.groovy
@@ -461,7 +461,7 @@ def ucdSnapshotEnvironment(deployContext, ucdToken, userInput) {
     def udClient = "./udclient/udclient"
     def ucdUrl = deployContext.deployment.ucd_url
     def ucdCmd = "${udClient} -authtoken ${ucdToken} -weburl ${ucdUrl}"
-    def setEnv = "-environment '${userInput.ucd_env}"
+    def setEnv = "-environment '${deployContext.env}"
     def setApp = "-application '${userInput.app_name}"
     def setName = "-name '${userInput.snapshot_name}"
     def ucdScript = "${ucdCmd} createSnapshotOfEnvironment ${setEnv} ${setApp} ${setName}"
