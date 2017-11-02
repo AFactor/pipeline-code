220b482d66ca582b29ac7c592ec01a91460b0fa8
Ram Guda <rguda@sapient.com>
2017-11-02 15:24:28 +0000

    Eagle deployment pipeline major upgrade. this release is not backwards compatible. use 3.7.1 tag for older release

--- src/com/lbg/workflow/sandbox/deploy/DeployContext.groovy
+++ src/com/lbg/workflow/sandbox/deploy/DeployContext.groovy
@@ -1,116 +1,29 @@
 package com.lbg.workflow.sandbox.deploy
 
-import groovy.json.JsonSlurperClassic
-
 class DeployContext implements Serializable {
 
-    HashMap schema
-    /**
-     * deployment journey
-     */
-    String journey
+    // release info
+    HashMap release
 
-    /**
-     * deployment environment - test, qa, stage
-     */
-    String env
-
-    /**
-     * deployment target - bluemix or urbancode?
-     */
-    String target
-
-    /**
-     * deployment metadata
-     */
-    HashMap metadata
-
-    /**
-     * list of services to deploy
-     */
+    // services - !!! do not be tricked, this is not a list of Service objects, but a list of HashMap !!!
     List<Service> services = new ArrayList<>()
 
-    /**
-     * deployment bluemix config
-     */
-    HashMap bluemix
-
-    /**
-     * api connect config
-     */
-    HashMap apiconnect
-
-    /**
-     * cmc config
-     */
-    HashMap cmc
-
-    /**
-     * deployment proxy config
-     */
-    HashMap proxy
-
-    /**
-     * ucd config
-     */
-    HashMap ucd
-
-    /**
-     * deployment restrict to label / node(s)
-     */
-    String label
-
-    /**
-     * deployment env
-     */
-    HashMap deployment
+    // platforms
+    HashMap platforms
 
     DeployContext() {
     }
 
-    DeployContext(String configuration) {
-        def config = (new HashMap(new JsonSlurperClassic().parseText(configuration))).asImmutable()
-        this.journey = config.journey
-        this.env = config.env
-        this.target = config.target
-        this.metadata = config.metadata
-        this.bluemix = config.bluemix
-        this.apiconnect = config.apiconnect
-        this.cmc = config.cmc
-        this.proxy = config.proxy
-        this.schema = config.schema
-        this.ucd = config.ucd
-        this.deployment = config.deployment
-        this.label = config.label
-
-        def dc = new DeployContext(config) // avoid jenkins serialization issues
-        this.services = dc.services
-
-        // Unfortunately this does not work - getting jenkins serialization errors
-        // leaving the code for now, hopefully when we upgrade our jenkins, we can revisit this.
-        // manually parse services & binaries
-//        ArrayList services = new ArrayList()
-//        for (def s in config.services) {
-//            Service service = new Service(s)
-//            services.add(service)
-//        }
-//        this.services = services
+    String releaseVersion() {
+        "${release.version.major}.${release.version.minor}.${release.version.patch}"
     }
 
     @Override
     String toString() {
         return "DeployContext{" +
-                "journey='" + journey + '\'' +
-                ", schema='" + schema + '\'' +
-                ", env='" + env + '\'' +
-                ", target='" + target + '\'' +
-                ", metadata='" + metadata + '\'' +
+                "release=" + release +
                 ", services=" + services +
-                ", bluemix=" + bluemix +
-                ", apiconnect=" + apiconnect +
-                ", cmc=" + cmc +
-                ", deployment=" + deployment +
-                ", proxy=" + proxy +
+                ", platforms=" + platforms +
                 '}'
     }
 }
