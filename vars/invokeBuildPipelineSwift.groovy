08131206ca695b1437093292815b1a93fc8d24df
Jacek Szlachta <jacek.slachta@sapient.com>
2018-02-07 11:04:57 +0000

    fix indentation

--- vars/invokeBuildPipelineSwift.groovy
+++ vars/invokeBuildPipelineSwift.groovy
@@ -3,15 +3,15 @@ import com.lbg.workflow.sandbox.Utils
 import com.lbg.workflow.sandbox.JobStats
 
 def call(String application, handlers, String configuration) {
-  this.call(application, handlers, configuration, 'lloydscjtdevops@sapient.com', 120)
+    this.call(application, handlers, configuration, 'lloydscjtdevops@sapient.com', 120)
 }
 
 def call(String application, handlers, String configuration, String notifyList) {
-  this.call(application, handlers, configuration, notifyList, 120)
+    this.call(application, handlers, configuration, notifyList, 120)
 }
 
 def call(String application, handlers, String configuration, Integer timeoutInMinutes) {
-  this.call(application, handlers, configuration, 'lloydscjtdevops@sapient.com', timeoutInMinutes)
+    this.call(application, handlers, configuration, 'lloydscjtdevops@sapient.com', timeoutInMinutes)
 }
 
 def call(String application,
@@ -20,93 +20,93 @@ def call(String application,
          String notifyList,
          Integer timeoutInMinutes) {
 
-  try {
-    echo "Start BuildPipelineHawk for ${configuration} / ${notifyList} / ${timeoutInMinutes}"
+    try {
+        echo "Start BuildPipelineHawk for ${configuration} / ${notifyList} / ${timeoutInMinutes}"
 
-    timeout(timeoutInMinutes) {
-      this.callHandler(application, handlers, configuration)
-      currentBuild.result = 'SUCCESS'
+        timeout(timeoutInMinutes) {
+            this.callHandler(application, handlers, configuration)
+            currentBuild.result = 'SUCCESS'
+        }
+
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        echo "BuildPipelineHawk caught exception [" + error.getMessage() + "]."
+        throw error
+
+    } finally {
+        if (notifyList?.trim()) {
+            emailNotify { to = notifyList }
+        }
+        echo "Finally invoke Splunk after " + currentBuild.result
+        def jobStats = new JobStats()
+        jobStats.toSplunk(env.BUILD_TAG, env.BUILD_URL, "jenkins-read-all", currentBuild.result, "")
     }
-
-  } catch (error) {
-    currentBuild.result = 'FAILURE'
-    echo "BuildPipelineHawk caught exception [" + error.getMessage() + "]."
-    throw error
-
-  } finally {
-    if (notifyList?.trim()) {
-      emailNotify { to = notifyList }
-    }
-    echo "Finally invoke Splunk after " + currentBuild.result
-    def jobStats = new JobStats()
-    jobStats.toSplunk(env.BUILD_TAG, env.BUILD_URL, "jenkins-read-all", currentBuild.result, "")
-  }
 
 }
 
 def callHandler(String application, handlers, String configuration) {
-  def targetCommit
-  def branch
-  BuildContext context
-  Utils utils = new Utils()
+    def targetCommit
+    def branch
+    BuildContext context
+    Utils utils = new Utils()
 
-  echo "Cleanup.."
-  step([$class: 'WsCleanup', notFailBuild: true])
+    echo "Cleanup.."
+    step([$class: 'WsCleanup', notFailBuild: true])
 
-  // TODO: better way to fix this ? no matching key exchange method found. Their offer: diffie-hellman-group1-sha1
-  fixGerritKeyExchangeIssue()
-  configureMavenSettings()
+    // TODO: better way to fix this ? no matching key exchange method found. Their offer: diffie-hellman-group1-sha1
+    fixGerritKeyExchangeIssue()
+    configureMavenSettings()
 
-  echo "Checking out from scm.."
-  checkout scm
+    echo "Checking out from scm.."
+    checkout scm
 
-  // env.BRANCH_NAME is only available in multibranch pipeline jobs
-  // to support scheduled pipeline jobs, we define and use local branch name
+    // env.BRANCH_NAME is only available in multibranch pipeline jobs
+    // to support scheduled pipeline jobs, we define and use local branch name
 
-  branch = env.BRANCH_NAME
-  if (branch == null) {
-    branch = sh(returnStdout: true, script: "git rev-parse --abbrev-ref HEAD").trim()
-  }
-  echo "Determine commit id.."
-  targetCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+    branch = env.BRANCH_NAME
+    if (branch == null) {
+        branch = sh(returnStdout: true, script: "git rev-parse --abbrev-ref HEAD").trim()
+    }
+    echo "Determine commit id.."
+    targetCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
 
-  context = new BuildContext(application, readFile(configuration))
+    context = new BuildContext(application, readFile(configuration))
 
-  if (isPatchsetBranch(branch)) {
+    if (isPatchsetBranch(branch)) {
 
-    echo "PatchsetWorkflow for ${targetCommit}.."
-    swiftPatchsetWorkflow(context, handlers, targetCommit)
+        echo "PatchsetWorkflow for ${targetCommit}.."
+        swiftPatchsetWorkflow(context, handlers, targetCommit)
 
-  } else if (isFeatureBranch(branch)) {
+    } else if (isFeatureBranch(branch)) {
 
-    branch1 = "ft-" + utils.friendlyName(branch, 20)
-    echo "FeatureWorkFlow for ${branch1}.."
-    swiftFeatureWorkflow(context, handlers, branch1)
+        branch1 = "ft-" + utils.friendlyName(branch, 20)
+        echo "FeatureWorkFlow for ${branch1}.."
+        swiftFeatureWorkflow(context, handlers, branch1)
 
-  } else if (isIntegrationBranch(branch)) {
+    } else if (isIntegrationBranch(branch)) {
 
-    branch1 = utils.friendlyName(branch, 40)
-    echo "IntegrationWorkFlow for ${branch1}.."
-    swiftIntegrationWorkflow(context, handlers, branch1)
+        branch1 = utils.friendlyName(branch, 40)
+        echo "IntegrationWorkFlow for ${branch1}.."
+        swiftIntegrationWorkflow(context, handlers, branch1)
 
-  } else {
-    error "No known git-workflow rule for branch called ${branch}"
-  }
-  echo "End BuildPipelineHawk for ${branch}"
+    } else {
+        error "No known git-workflow rule for branch called ${branch}"
+    }
+    echo "End BuildPipelineHawk for ${branch}"
 }
 
 private fixGerritKeyExchangeIssue() {
-  sh 'mkdir -p $HOME/.ssh && touch $HOME/.ssh/config && \
+    sh 'mkdir -p $HOME/.ssh && touch $HOME/.ssh/config && \
         echo -en  "Host gerrit.sandbox.extranet.group\nKexAlgorithms +diffie-hellman-group1-sha1" > $HOME/.ssh/config'
 }
 
 private configureMavenSettings() {
-  sh('mkdir -p $HOME/.m2')
-  sh("echo -en '${mavenSettingsText()}' > \$HOME/.m2/settings.xml")
+    sh('mkdir -p $HOME/.m2')
+    sh("echo -en '${mavenSettingsText()}' > \$HOME/.m2/settings.xml")
 }
 
 private mavenSettingsText() {
-  '''
+    '''
 <settings>
     <servers>
         <server>
--- vars/swiftIntegrationWorkflow.groovy
+++ vars/swiftIntegrationWorkflow.groovy
@@ -1,172 +1,172 @@
 import com.lbg.workflow.sandbox.BuildContext
 
 def call(BuildContext context, handlers, String targetBranch) {
-	def unitTests = []
-	def sanityTests = []
-	def integrationTests = []
-	def allTests = []
+    def unitTests = []
+    def sanityTests = []
+    def integrationTests = []
+    def allTests = []
 
-	def builder
-	def appDeployer
+    def builder
+    def appDeployer
 
-	def success = false
+    def success = false
 
-	def targetEnv = "integration"
+    def targetEnv = "integration"
 
-	def epoch
+    def epoch
 
-	String integrationEnvironment = "${context.application}-${targetBranch}"
+    String integrationEnvironment = "${context.application}-${targetBranch}"
 
-	stage("Initialize") {
-		try {
-			echo "TARGET_BRANCH: ${targetBranch}"
-			epoch = sh(returnStdout: true, script: 'date +%d%m%Y%H%M').trim()
+    stage("Initialize") {
+        try {
+            echo "TARGET_BRANCH: ${targetBranch}"
+            epoch = sh(returnStdout: true, script: 'date +%d%m%Y%H%M').trim()
 
-			echo "Loading all handlers"
-			echo "Loading Builder: ${handlers.builder}"
-			builder = loadBuildHandler("${handlers.builder}")
+            echo "Loading all handlers"
+            echo "Loading Builder: ${handlers.builder}"
+            builder = loadBuildHandler("${handlers.builder}")
 
-			echo "Loading Deployer: ${handlers.deployer}"
-			appDeployer = loadBuildHandler(handlers.deployer)
+            echo "Loading Deployer: ${handlers.deployer}"
+            appDeployer = loadBuildHandler(handlers.deployer)
 
-			for (String test : handlers.getUnitTests()) {
-				echo "Loading ${test}"
-				unitTests.add(loadBuildHandler("${test}"))
-			}
-			for (String test : handlers.getStaticAnalysis()) {
-				echo "Loading ${test}"
-				sanityTests.add(loadBuildHandler("${test}"))
-			}
-			for (String test : handlers.getIntegrationTests()) {
-				echo "Loading ${test}"
-				integrationTests.add(loadBuildHandler("${test}"))
-			}
+            for (String test : handlers.getUnitTests()) {
+                echo "Loading ${test}"
+                unitTests.add(loadBuildHandler("${test}"))
+            }
+            for (String test : handlers.getStaticAnalysis()) {
+                echo "Loading ${test}"
+                sanityTests.add(loadBuildHandler("${test}"))
+            }
+            for (String test : handlers.getIntegrationTests()) {
+                echo "Loading ${test}"
+                integrationTests.add(loadBuildHandler("${test}"))
+            }
 
-			allTests.addAll(unitTests)
-			allTests.addAll(sanityTests)
-			allTests.addAll(integrationTests)
-		} catch (error) {
-			echo error.message
-			throw error
-		} finally {
-		}
-		milestone(label: 'Ready')
-	}
-	try {
-		// Basic Qualification -----------------------------------//
-		if (!unitTests.empty) {
-			stage("Unit Tests") {
-				for (Object testClass : unitTests) {
-					def currentTest = testClass
-					currentTest.runTest(targetBranch, context)
-				}
-			}
-			milestone(label: 'UnitTests')
-		}
+            allTests.addAll(unitTests)
+            allTests.addAll(sanityTests)
+            allTests.addAll(integrationTests)
+        } catch (error) {
+            echo error.message
+            throw error
+        } finally {
+        }
+        milestone(label: 'Ready')
+    }
+    try {
+        // Basic Qualification -----------------------------------//
+        if (!unitTests.empty) {
+            stage("Unit Tests") {
+                for (Object testClass : unitTests) {
+                    def currentTest = testClass
+                    currentTest.runTest(targetBranch, context)
+                }
+            }
+            milestone(label: 'UnitTests')
+        }
 
-		// Sonar/Checkstyle et al -----------------------------------//
-		if (!sanityTests.empty) {
-			stage("Static Analysis") {
-				def codeSanitySchedule = [:]
-				for (Object testClass : sanityTests) {
-					def currentTest = testClass
-					codeSanitySchedule[currentTest.name()] = { currentTest.runTest(targetBranch, context) }
-				}
-				try {
-					parallel codeSanitySchedule
-					milestone(label: 'StaticAnalysis')
-				} catch (error) {
-					echo "Static Analysis has failed."
-					throw error
-				} finally {
-					//Make a decision
-				}
-			}
-		}
+        // Sonar/Checkstyle et al -----------------------------------//
+        if (!sanityTests.empty) {
+            stage("Static Analysis") {
+                def codeSanitySchedule = [:]
+                for (Object testClass : sanityTests) {
+                    def currentTest = testClass
+                    codeSanitySchedule[currentTest.name()] = { currentTest.runTest(targetBranch, context) }
+                }
+                try {
+                    parallel codeSanitySchedule
+                    milestone(label: 'StaticAnalysis')
+                } catch (error) {
+                    echo "Static Analysis has failed."
+                    throw error
+                } finally {
+                    //Make a decision
+                }
+            }
+        }
 
-		// Build--------------------------------------------------//
-		stage("Package") {
-			builder.pack(targetBranch, targetEnv, context)
-		}
-		milestone(label: 'Build')
+        // Build--------------------------------------------------//
+        stage("Package") {
+            builder.pack(targetBranch, targetEnv, context)
+        }
+        milestone(label: 'Build')
 
-		// Concurrency Controlled Deploy/IntegrationTest Cycle-----------------//
-		lock(inversePrecedence: true, quantity: 1, resource: integrationEnvironment) {
-			// Integration Tests--------------------------------------//
-			if (!integrationTests.empty) {
-				stage("Deploy") {
-					appDeployer.deploy(targetBranch, context)  //Hardcoded to DEV as current practice
-				}
-				// Integration Tests--------------------------------------//
-				stage("Integration Tests") {
-					def integrationTestSchedule = [:]
+        // Concurrency Controlled Deploy/IntegrationTest Cycle-----------------//
+        lock(inversePrecedence: true, quantity: 1, resource: integrationEnvironment) {
+            // Integration Tests--------------------------------------//
+            if (!integrationTests.empty) {
+                stage("Deploy") {
+                    appDeployer.deploy(targetBranch, context)  //Hardcoded to DEV as current practice
+                }
+                // Integration Tests--------------------------------------//
+                stage("Integration Tests") {
+                    def integrationTestSchedule = [:]
 
-					for (Object testClass : integrationTests) {
-						def currentTest = testClass
-						integrationTestSchedule[currentTest.name()] = { currentTest.runTest(targetBranch, context) }
-					}
-					try {
-						parallel integrationTestSchedule
-						milestone(label: 'IntegrationTests')
-					} catch (error) {
-						echo "Integration tests failed"
-						throw error
-					} finally {
-						//Make a decision
-					}
-				}
-			}
+                    for (Object testClass : integrationTests) {
+                        def currentTest = testClass
+                        integrationTestSchedule[currentTest.name()] = { currentTest.runTest(targetBranch, context) }
+                    }
+                    try {
+                        parallel integrationTestSchedule
+                        milestone(label: 'IntegrationTests')
+                    } catch (error) {
+                        echo "Integration tests failed"
+                        throw error
+                    } finally {
+                        //Make a decision
+                    }
+                }
+            }
 
-			success = true
-		}
-	} catch (error) {
-		echo "Some Mandatory Steps have failed. Aborting Build"
-		throw error
-	} finally {
-		// Clean up environments/workspaces ----------------------//
-		stage("Cleanup") {
-			try {
-				appDeployer.purge(targetBranch, context)
-			} catch (error) {
-				echo "Notice: Cleanup failed. Onwards!"
-			} finally {
-			}
-		}
+            success = true
+        }
+    } catch (error) {
+        echo "Some Mandatory Steps have failed. Aborting Build"
+        throw error
+    } finally {
+        // Clean up environments/workspaces ----------------------//
+        stage("Cleanup") {
+            try {
+                appDeployer.purge(targetBranch, context)
+            } catch (error) {
+                echo "Notice: Cleanup failed. Onwards!"
+            } finally {
+            }
+        }
 
-		// Publish to 3rd Party Stacks----------------------------//
-		stage("Publish") {
-			if (success) {
-				try {
-					builder.publishNexus(targetBranch, targetEnv, context)
-				} catch (error) {
-					echo error.message
-					echo "Nexus publication did not complete normally. Continuing"
-					throw error
-				} finally {
+        // Publish to 3rd Party Stacks----------------------------//
+        stage("Publish") {
+            if (success) {
+                try {
+                    builder.publishNexus(targetBranch, targetEnv, context)
+                } catch (error) {
+                    echo error.message
+                    echo "Nexus publication did not complete normally. Continuing"
+                    throw error
+                } finally {
 
-				}
-			} else {
-				echo "Build has failed. Not publishing to nexus"
-			}
-			//Publish what you can to splunk regardless of success.
-			if (!allTests.empty) {
-				try {
-					splunkPublisher {
-						tests = allTests
-						timestamp = epoch
-						buildContext = context
-						branchName = targetBranch
-					}
-				} catch (error) {
-					echo error.message
-					echo "Splunk report publication did not complete normally. Continuing"
-				} finally {
-				}
-			}
+                }
+            } else {
+                echo "Build has failed. Not publishing to nexus"
+            }
+            //Publish what you can to splunk regardless of success.
+            if (!allTests.empty) {
+                try {
+                    splunkPublisher {
+                        tests = allTests
+                        timestamp = epoch
+                        buildContext = context
+                        branchName = targetBranch
+                    }
+                } catch (error) {
+                    echo error.message
+                    echo "Splunk report publication did not complete normally. Continuing"
+                } finally {
+                }
+            }
 
-		}
-		stage("End") { echo "Phew! Finally finished." }
-	}
+        }
+        stage("End") { echo "Phew! Finally finished." }
+    }
 }
 
 return this
