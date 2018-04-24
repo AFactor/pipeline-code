26e03b76fb84ec53f24b773bd775b1c3c82bb5af
Ram Guda <rguda@sapient.com>
2018-04-24 14:06:03 +0100

    Upgrade Vault token shared lib to work with new Vault plugin. Old method still supported but will fail when using newer versions of vault plugin

--- vars/withGenericVaultSecrets.groovy
+++ vars/withGenericVaultSecrets.groovy
@@ -4,6 +4,50 @@
  */
 import com.lbg.workflow.sandbox.ServiceDiscovery
 
+def call(String vaultCredentialsId, Map<String,String> mapping, Closure body) {
+
+	if (! vaultCredentialsId) {
+		error "withGenericVaultSecrets: Cannot inject vault secrets without VaultCredentialsId."
+	}
+
+	def locator = new ServiceDiscovery()
+	def secretList = []
+
+	for( item in mapping){
+
+		def pathTokens = item.value.split('/')
+		def vaultKey = pathTokens.last()
+		def path = pathTokens[0]
+		for (int i = 1; i< pathTokens.size()-1; i++){
+			path += '/'
+			path += pathTokens[i]
+		}
+		def envVar = item.key
+
+		echo "Mapping Secret:${vaultKey} from vault:${path} to ${envVar}"
+		secretList.add([$class: 'VaultSecret',
+						path: "${path}",
+						secretValues: 	[
+								[ 	$class: 'VaultSecretValue',
+									 envVar: "${envVar}", vaultKey: "${vaultKey}"
+								]
+						]
+		])
+	}
+
+	def configuration = [$class: 'VaultConfiguration',
+						 vaultUrl: locator.locate('vault'),
+						 vaultCredentialId: vaultCredentialsId]
+
+	wrap([$class: 'VaultBuildWrapper', configuration: configuration, vaultSecrets: secretList]) {
+		body()
+	}
+}
+
+/**
+ * Works only with the older Vault Plugin. Move to the above method.
+ */
+@Deprecated
 def call(Map<String,String> mapping, Closure body) {
 
 	if (! env.VAULT_TOKEN) {
@@ -43,3 +87,5 @@ def call(Map<String,String> mapping, Closure body) {
 		body()
 	}
 }
+
+
