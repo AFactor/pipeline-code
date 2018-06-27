b23997edaae85921df8559ed43fa49e620bd9674
stephankfolkes <stephan.folkes@sapient.com>
2018-06-27 15:42:06 +0100

    Adding extra decimal in eagleUcdCreateEnvironmentSnapshotService.groovy
    Change-Id: I7b34f3acfa0440ca524fac6b3bd0dd39185d64b8

--- vars/eagleUcdCreateEnvironmentSnapshotService.groovy
+++ vars/eagleUcdCreateEnvironmentSnapshotService.groovy
@@ -71,8 +71,8 @@ private createEnvironmentSnapshotJson(artifactVersions, appName, snapshotName, c
 }
 
 private environmentSnapshotName(sourceSnapshotName, environment, buildNumber) {
-    def sourceSnapshotNameWithoutBuildNumber = sourceSnapshotName.substring(0, sourceSnapshotName.lastIndexOf("."))
-    "${environment}.${sourceSnapshotNameWithoutBuildNumber}.${buildNumber}"
+    def sourceSnapshotNameWithoutBuildNumberAndWithoutPriorEnv = sourceSnapshotName.substring(sourceSnapshotName.indexOf(".")+1, sourceSnapshotName.lastIndexOf("."))
+    "${environment}.${sourceSnapshotNameWithoutBuildNumberAndWithoutPriorEnv}.${buildNumber}"
 }
 
 private printSummary(snapshotName) {
