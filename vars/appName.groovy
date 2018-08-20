74afb48a5877a06c875f8cdd7d09b980baf0a37a
Harpal Dhillon <hdhillon@sapient.com>
2018-08-20 12:24:11 +0100

    New overloaded functions added in App name

--- vars/appName.groovy
+++ vars/appName.groovy
@@ -12,3 +12,29 @@ def call(String application, String targetBranch){
 def call(BuildContext context, String targetBranch){
 	return "j2-${context.application}-${targetBranch}"
 }
+
+def call(BuildContext context, String targetBranch, Integer custom_app_length){
+   int size = context.application.trim().size()
+   if (size > custom_app_length) {
+       newAppName=context.application.trim().replaceAll("\\.","-").replaceAll("_","-").replaceAll("-+","-")[0..custom_app_length].replaceAll("^-+","").replaceAll("-+\$","")
+   } else {
+       newAppName=context.application.trim().replaceAll("\\.","-").replaceAll("_","-").replaceAll("-+","-").replaceAll("^-+","").replaceAll("-+\$","")
+   }
+ 
+   newTargetBranch=targetBranch.trim().replaceAll("\\.","-").replaceAll("_","-").replaceAll("-+","-").replaceAll("^-+","").replaceAll("-+\$","")
+   return "j2-${newAppName}-${newTargetBranch}"
+}
+ 
+ 
+def call(String application, String targetBranch, Integer custom_app_length){
+ 
+   int size = application.trim().size()
+   if (size > custom_app_length) {
+       newAppName=application.trim().replaceAll("\\.","-").replaceAll("_","-").replaceAll("-+","-")[0..custom_app_length].replaceAll("^-+","").replaceAll("-+\$","")
+   } else {
+       newAppName=application.trim().replaceAll("\\.","-").replaceAll("_","-").replaceAll("-+","-").replaceAll("^-+","").replaceAll("-+\$","")
+   }
+ 
+   newTargetBranch=targetBranch.trim().replaceAll("\\.","-").replaceAll("_","-").replaceAll("-+","-").replaceAll("^-+","").replaceAll("-+\$","")
+   return "j2-${newAppName}-${newTargetBranch}"
+}
