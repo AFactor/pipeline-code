c43bae4bc61dc5b5a752983fc253f742f5978fde
Alex Knowles <alex.knowles@sapient.com>
2018-08-30 13:52:21 +0100

    feature/check-digest
    Squashed commit of the following:
    
    commit 97b4ddea1746b25ff391bfe2a5a083aa32ffd33c
    Merge: f74fe7b 01d3d1a
    Author: Alex Knowles <alex.knowles@sapient.com>
    Date:   Thu Aug 30 13:51:56 2018 +0100
    
        Merge branch 'master' into feature/check-digest
    
    commit f74fe7bf5a388d3525f6c7d554f33b1f3bce1dd7
    Author: Alex Knowles <alex.knowles@sapient.com>
    Date:   Thu Aug 30 13:12:26 2018 +0100
    
        missing (
    
    commit d623b12514da0234c886d317e884a1cf83dba1e9
    Author: Alex Knowles <alex.knowles@sapient.com>
    Date:   Thu Aug 30 13:10:33 2018 +0100
    
        missing )
    
    Change-Id: I2c38d0ff33ab13c76e9648b3ac297b02416b1654

--- vars/eagleDeployOpenAMService.groovy
+++ vars/eagleDeployOpenAMService.groovy
@@ -1,3 +1,5 @@
+import com.lbg.workflow.global.GlobalUtils
+
 def call(service, deployContext) {
 	if (service.type == "Liberty") {
 		if (needsDeployment(service, deployContext)) {
@@ -11,13 +13,15 @@ def call(service, deployContext) {
 			def appName = "${deployContext.release.journey}-${service.name}-${deployContext.release.environment}"
 			dir(service.name) {
 				def artifactName = sh(script: "ls *.zip| head -1", returnStdout: true).trim()
-				def appHostName = sh(script: "hostname", returnStdout: true).trim()
-				def appPort = service.platforms.openam['docker-port']
-				sh "unzip ${artifactName} wlp/usr/servers/* wlp/dev/tools/openam/*.sh "
 				def tokens = buildTokens(service, deployContext)
-				tokens['OPENAM_DOMAIN'] = "${appHostName}"
-				tokens['OPENAM_SERVER_URL'] = "http://${appHostName}:${appPort}"
-				tokens['OPENAM_LB_PRIMARY_URL'] = "http://${appHostName}:${appPort}/access-mgmt-service"
+                def appHostName = sh(script: "hostname", returnStdout: true).trim()
+                def appPort = service.platforms.openam['docker-port']
+                tokens['OPENAM_DOMAIN'] = "${appHostName}"
+                tokens['OPENAM_SERVER_URL'] = "http://${appHostName}:${appPort}"
+                tokens['OPENAM_LB_PRIMARY_URL'] = "http://${appHostName}:${appPort}/access-mgmt-service"
+
+				sh "unzip ${artifactName} wlp/usr/servers/* wlp/dev/tools/openam/*.sh "
+
 				replaceTokens('wlp/usr/servers', tokens)
 				replaceTokens('wlp/dev/tools/openam', tokens)
 				sh "zip ${artifactName}  wlp -r"
@@ -29,7 +33,8 @@ def call(service, deployContext) {
 						"ARTIFACT_VERSION=${getArtifactVersion(artifact)}",
 						"OPENAM_DIRECTORY_PORT=${tokens['OPENAM_DIRECTORY_PORT']}",
 						"OPENAM_DIRECTORY_ADMIN_PORT=${tokens['OPENAM_DIRECTORY_ADMIN_PORT']}",
-						"OPENAM_DIRECTORY_JMX_PORT=${tokens['OPENAM_DIRECTORY_JMX_PORT']}"
+						"OPENAM_DIRECTORY_JMX_PORT=${tokens['OPENAM_DIRECTORY_JMX_PORT']}",
+                        "TOKENS_DIGEST=${getTokensDigest(service, deployContext)}"
 				]) {
 					try {
 						def dockerFile = libraryResource 'com/lbg/workflow/sandbox/openam/Dockerfile'
@@ -56,15 +61,23 @@ def needsDeployment(service, deployContext) {
 	def appName = "${deployContext.release.journey}-${service.name}-${deployContext.release.environment}"
 	def artifact = service.runtime.binary.artifact
 	def artifactVersion = getArtifactVersion(artifact)
+    def tokensDigest = getTokensDigest(service, deployContext)
 	withEnv([
 			"APP=${appName}",
 			"ARTIFACT_VERSION=${artifactVersion}"
 	]) {
 		try {
-			String result = sh(script: "docker exec \$APP  sh -c 'printf \$ARTIFACT_VERSION'", returnStdout: true, returnStatus: false).trim()
-			def resultVersion = (result ==~ /(?s)(.*)($artifactVersion)(.*)/)
-			echo "version match: <$resultVersion>"
-			return !(resultVersion)
+			String result = sh(script: "docker exec \$APP  sh -c 'printf \$ARTIFACT_VERSION-\$TOKENS_DIGEST'", returnStdout: true, returnStatus: false).trim()
+            if( !(result ==~ /(?s)(.*)($artifactVersion)(.*)/) ||
+                !(result ==~ /(?s)(.*)($tokensDigest)(.*)/)
+                ) {
+                    echo "app check - deploy app: <$appName>"
+                    return true
+                } else {
+                    echo "skip deploy app: <$appName>"
+                    return false
+                }
+
 		} catch (error) {
 			echo "needs deployment failed $error.message"
 			return true
@@ -89,4 +102,14 @@ private def buildTokens(service, deployContext) {
 	tokens.putAll(service?.platforms?.openam?.tokens ?: [:])
 	tokens.putAll(service?.tokens ?: [:])
 	return tokens
+}
+
+def getTokensDigest(service, deployContext) {
+	def tokens = buildTokens(service, deployContext)
+	GlobalUtils utils = new GlobalUtils()
+	def sortedTokens = utils.sortMap(tokens)
+	echo "Sorted tokens successfully"
+	def digest = utils.generateDigest("SHA-512", sortedTokens.toString())
+	echo "digest: ${digest}"
+	return digest
 }
