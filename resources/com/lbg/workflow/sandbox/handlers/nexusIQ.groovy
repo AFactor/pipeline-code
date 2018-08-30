b7f081a81c41c584e230bf1e1985615ae76ad138
Michal Tekel <mtekel@gmail.com>
2018-08-30 16:11:47 +0100

    add-nexusIQ-breakers
    Squashed commit of the following:
    
    commit 2755b5b772f9bebcfbc82393e6931c52eb71256f
    Author: Michal Tekel <mtekel@gmail.com>
    Date:   Thu Aug 30 16:11:45 2018 +0100
    
        Remove unused variable
    
    commit 1122934579376f343f1f3262fba1cfb0858c61a5
    Author: Michal Tekel <mtekel@gmail.com>
    Date:   Thu Aug 30 16:07:29 2018 +0100
    
        Remove debug
    
    commit cc18ec4dfeb338aaeaef0ad0d0d8da25c978d267
    Author: Michal Tekel <mtekel@gmail.com>
    Date:   Thu Aug 30 15:58:55 2018 +0100
    
        REVERT: add debug
    
    commit 294ea8b9703b722bb646cca4b9a3b42f091c4bf9
    Author: Michal Tekel <mtekel@gmail.com>
    Date:   Thu Aug 30 15:32:15 2018 +0100
    
        nxIQ: improve error reporting
    
    commit c053cc5bd4151a089342d498433fbe4940a25889
    Author: Michal Tekel <mtekel@gmail.com>
    Date:   Thu Aug 30 15:24:01 2018 +0100
    
        nxIQ: improve error reporting
    
    commit e8dba2ebfde493cf19d525ba29b2709cd7344295
    Author: Michal Tekel <mtekel@gmail.com>
    Date:   Thu Aug 30 15:08:35 2018 +0100
    
        Fix results writing
    
    commit 4e72192d2d5d62010e5aac2ec1861625cc8eeca9
    Author: Michal Tekel <mtekel@gmail.com>
    Date:   Thu Aug 30 14:53:14 2018 +0100
    
        Fix echo output
    
    commit b6e01ac78ec758647bfe48895d4ba1299bd3a28e
    Author: Michal Tekel <mtekel@gmail.com>
    Date:   Thu Aug 30 14:09:45 2018 +0100
    
        New nexusIQ scan features
    
        * Added build breakers (configurable)
        * Create evaluation result json
        * Make json files human readable
        * Send to splunk evaluation results instead of detailed report
    
    Change-Id: I87a1b7cde6497add4904f64d2f60e92e1c6927e0

--- resources/com/lbg/workflow/sandbox/handlers/nexusIQ.groovy
+++ resources/com/lbg/workflow/sandbox/handlers/nexusIQ.groovy
@@ -1,4 +1,6 @@
 import groovy.json.JsonSlurperClassic
+import groovy.json.JsonOutput
+
 /*
  * Runs nexusIQ scan on the provided artifact. All config is read from main
  * job config context specified usually in pipelines/conf/job-config.json
@@ -13,13 +15,19 @@ import groovy.json.JsonSlurperClassic
 
 def runTest(String targetBranch, context){
 	def cfg = getConfig(targetBranch, context)
+	def fail = false
 
 	if (cfg.runScan){
 		// lock so that we can retrieve report for current branch before next scan begins
 		lock(quantity: 1, resource: cfg.iqApp){
 			createApp(cfg)
-			runScan(cfg)
+			fail = runScan(cfg)
 			getReport(cfg)
+
+			// always get detailed report first before potentially breaking the build
+			if (fail){
+				error("nexusIQ: One of build breaker thresholds reached. See scan logs for details.")
+			}
 		}
 	} else {
 		print "Scan not enabled for ${targetBranch}, skipping nexusIQ analysis..."
@@ -30,8 +38,8 @@ def publishSplunk(targetBranch, epoch, context, handler){
 	def cfg = getConfig(targetBranch, context)
 
 	if (cfg.runScan){
-		unstash "nexusIqReport"
-		handler.SCP(cfg.report, "${cfg.splunkDir}/${epoch}_${cfg.report}")
+		unstash "nexusIqResult"
+		handler.SCP(cfg.result, "${cfg.splunkDir}/${epoch}_${cfg.result}")
 	} else {
 		print "No nexusIQ scan was run, nothing to publish."
 	}
@@ -47,14 +55,21 @@ def getConfig(String targetBranch, context){
 	def cfg=[:]
 
 	cfg.iqApp         = context.application
-	cfg.report        = nxIQctx?.reportFile?:  "${cfg.iqApp}-nexusIQ.json"
+	cfg.report        = nxIQctx?.reportFile?:  "${cfg.iqApp}-IQreport.json"
+	cfg.result        = nxIQctx?.resultFile?:  "${cfg.iqApp}-IQresult.json"
 	cfg.iqStage       = nxIQctx?.stage?:       'build'
 	cfg.credsId       = nxIQctx?.credentials?: 'NexusIQ-SRVAPPOSSJNKOB01'
 	cfg.scanPattern   = nxIQctx?.scanPattern?: '*'
 	cfg.artifactStash = nxIQctx?.stash?:       'artifactStash'
 	cfg.nodeLabel     = nxIQctx?.nodeLabel?:   'nexusIQ'
 	cfg.apiNode       = nxIQctx?.apiLabel?:    'lbg_slave'
-	cfg.splunkDir	  = nxIQctx?.reportdir?:   context.config.splunk.reportdir
+	cfg.splunkDir     = nxIQctx?.reportdir?:   context.config.splunk.reportdir
+
+	// Generous default build breakers before we adopt any standards
+	cfg.affected      = nxIQctx?.affected?:    100000
+	cfg.critical      = nxIQctx?.critical?:    100000
+	cfg.severe        = nxIQctx?.severe?:      100000
+	cfg.moderate      = nxIQctx?.moderate?:    100000
 
 	cfg.nexusIqApi    = nxIQctx?.api?:   "https://nexusiqapp.service.group:8070/api/v2"
 	cfg.orgID         = nxIQctx?.orgID?: "d101c545f626458f849403cfed83d709"  // ENGR Open Banking
@@ -100,22 +115,60 @@ def createApp(cfg){
 }
 
 def runScan(cfg){
+	def failed = false
 	node(cfg.nodeLabel){
 		unstash cfg.artifactStash
 		print "About to initiate nexusIQ scan..."
 
 		try {
-			nexusPolicyEvaluation failBuildOnNetworkError: true,
+			results = nexusPolicyEvaluation failBuildOnNetworkError: true,
 				iqApplication: cfg.iqApp,
 				iqScanPatterns: [[scanPattern: cfg.scanPattern]],
 				iqStage: cfg.iqStage,
 				jobCredentialsId: cfg.credsId
+
+			// evaluate build breakers
+			if (results.affectedComponentCount > cfg.affected) { failed = true
+				print "${results.affectedComponentCount} affected is more than limit of ${cfg.affected}" }
+
+			if (results.criticalComponentCount > cfg.critical) { failed = true
+				print "${results.criticalComponentCount} critical is more than limit of ${cfg.critical}" }
+
+			if (results.severeComponentCount > cfg.severe)     { failed = true
+				print   "${results.severeComponentCount} severe is more than limit of ${cfg.severe}" }
+
+			if (results.moderateComponentCount > cfg.moderate) { failed = true
+				print "${results.moderateComponentCount} moderate is more than limit of ${cfg.moderate}" }
+
+			if (failed){
+				print "One of build breaker thresholds reached. Failing build after obtaining detailed scan report."
+			}
+
+			// Show real report url which is on nexusiquser.intranet.group
+			def reportUrl = results.applicationCompositionReportUrl.replaceFirst(
+				"nexusiqapp.service.group", "nexusiquser.intranet.group")
+			print "Fixed report url: ${reportUrl}"
+
+			// Prepare evaluation results json
+			def evalResults = JsonOutput.toJson(
+				affected: results.affectedComponentCount,
+				critical: results.criticalComponentCount,
+				severe:   results.severeComponentCount,
+				moderate: results.moderateComponentCount,
+				report:   reportUrl
+			)
+			writeFile file: cfg.result, text: evalResults
+
+			stash name: "nexusIqResult", includes: cfg.result
+			archiveArtifacts cfg.result
+			step([$class: 'WsCleanup', notFailBuild: true])
 		} catch (e){
 			print "NexusIQ scan failed: "
 			print e
 			throw e
 		}
 	}
+	return failed
 }
 
 def getReport(cfg){
@@ -146,7 +199,7 @@ def getReport(cfg){
 		try {
 			// remove api/v2 from reports url as that is already part of cfg.nexusIqApi
 			reportUrl = repUrl.replaceFirst("api/v2","")
-			getReport = iqCurl("${cfg.nexusIqApi}/${reportUrl} >${cfg.report}", cfg)
+			getReport = iqCurl("${cfg.nexusIqApi}/${reportUrl} | python -mjson.tool >${cfg.report}", cfg)
 		} catch (e){
 			print "Got error while getting report:"
 			print e.message
@@ -154,7 +207,6 @@ def getReport(cfg){
 		}
 
 		archiveArtifacts cfg.report
-		stash name: "nexusIqReport", includes: cfg.report
 		step([$class: 'WsCleanup', notFailBuild: true])
 	}
 }
